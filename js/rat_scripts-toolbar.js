042a8c0bb8dc04008103eea539cbc211c6d6e752
netdesignr <michaiel.webdesigner@gmail.com>
2018-12-29 15:43:48 +0000

    Remove files

--- js/rat_scripts-toolbar.js
+++
@@ -1,9 +0,0 @@
-/**
- * Reset settings button
- */
-
-jQuery(document).ready(function(){
-    jQuery('.rat-toolbar input[value="Reset Settings"]').on('click', function () {
-        jQuery('.rat-toolbar input[name="rat-toolbar-radio"]').prop('checked', false);
-    });
-});
